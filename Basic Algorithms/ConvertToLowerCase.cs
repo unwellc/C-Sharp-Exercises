58b515caf184d835b05575710300747f29104de2
unwellc <106143619+unwellc@users.noreply.github.com>
2022-05-29 22:40:41 +0800

    Create ConvertToLowerCase.cs

---
+++ Basic Algorithms/ConvertToLowerCase.cs
@@ -0,0 +1,8 @@
+//Converts string to lower case
+class Program {
+    static void Main(string[] args) {
+      Console.Write("Input: ");
+      string input = Console.ReadLine();
+      Console.WriteLine(input.ToLower());
+    }
+}
