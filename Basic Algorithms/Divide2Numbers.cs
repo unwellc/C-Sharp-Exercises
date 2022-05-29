a2f27546e681fca3c4c7b0259aa99c3c8e5d7f43
unwellc <106143619+unwellc@users.noreply.github.com>
2022-05-29 22:08:20 +0800

    Create Divide2Numbers.cs

---
+++ Basic Algorithms/Divide2Numbers.cs
@@ -0,0 +1,10 @@
+//Divide 2 numbers
+public class Exercise1
+{
+    public static void Main( )
+    {
+        int num1 = 12;
+        int num2 = 6;
+        System.Console.WriteLine(num1 / num2);
+    }
+}
