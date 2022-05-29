eb7ea204ac189b7bb424942ac7d4c2e93658a418
unwellc <106143619+unwellc@users.noreply.github.com>
2022-05-29 22:07:22 +0800

    Create PrintingString.cs

---
+++ Basic Algorithms/PrintingString.cs
@@ -0,0 +1,11 @@
+// Print Hello and Name in separate line
+public class Exercise1
+{
+    public static void Main( )
+    {
+         string name = "Anwille Catindig"; 
+         System.Console.WriteLine("Hello:");
+         System.Console.WriteLine(name);
+        
+    }
+}
